83936b26ad6da692f43121ecf569d1cd4325edfe
Luc Perkins <lucperkins@gmail.com>
2022-07-29 03:26:16 +0200

    Allow for setting port via env var

--- go/cmd/web/main.go
+++ go/cmd/web/main.go
@@ -1,17 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
+func getPort() string {
+  if os.Getenv("PORT") != "" {
+    return os.Getenv("PORT")
+  } else {
+    return "1111"
+  }
+}
+
 func main() {
   srv := gin.Default()
+
+  addr := fmt.Sprintf(":%s", getPort())
 
   srv.GET("/hello", func(c *gin.Context) {
     c.String(http.StatusOK, "Welcome to Go inside Docker built with Nix!")
   })
 
-  srv.Run(":1111")
+  srv.Run(addr)
 }
